c221587034ecd2245946c9c5fb6c99fd3099c14b
Luis Estigarribia <luisalfredoestigarribiasosa85@gmail.com>
2025-10-26 14:47:10 -0300

    Configure solid_cache and solid_queue connections in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,10 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
+  config.solid_cache.connects_to = { database: { writing: :cache } }
+
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to  = { database: { writing: :queue } }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
