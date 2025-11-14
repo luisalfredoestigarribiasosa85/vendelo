a5ce6e82ead60c60da86eb7acc9ca8d293aa392c
Luis Alfredo <luisalfredoestigarribiasosa85@gmail.com>
2025-11-13 21:49:05 -0300

    fix translations missing and description error of saving correctly the category's description

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -66,6 +66,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.expect(category: [ :name ])
+      params.expect(category: [ :name, :description ])
     end
 end
